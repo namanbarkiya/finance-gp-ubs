416d58e44d209dc7fb8bdb2765cef12b25cba6c2
Naman <naman.barkiya02@gmail.com>
2024-09-13 15:30:17 +0530

    added tanstack table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import SampleTable from "./pages/SampleTable";
 
 function App() {
     return (
         <>
             <h1 className="text-3xl">GP Finance</h1>
+            <SampleTable />
         </>
     );
 }
