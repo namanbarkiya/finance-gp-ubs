914ab66f743815082a8470e963aeff7efd5daff8
Naman <naman.barkiya02@gmail.com>
2024-09-15 16:49:45 +0530

    UI improvements

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,29 @@ import ClearingMemberBalance from "./components/ClearingMemberBalance";
 import CorporateActionButton from "./components/CorporateActionButton";
 
 function App() {
-
-  const [trades, setTrades] = useState([]);
-  const [balances, setBalances] = useState({});
-  const [corporateActions, setCorporateActions] = useState([]);
+    const [trades, setTrades] = useState([]);
+    const [balances, setBalances] = useState({});
+    const [corporateActions, setCorporateActions] = useState([]);
 
     return (
         <>
-            <h1 className="text-3xl">Multilateral Netting System</h1>
-            <br/>
+            <h1 className="text-3xl font-bold underline">
+                Multilateral Netting System
+            </h1>
+            <br />
 
-            <TradeList trades={trades} setTrades={setTrades}/>
-            <br/>
+            <TradeList trades={trades} setTrades={setTrades} />
+            <br />
 
-            <ClearingMemberBalance balances={balances} setBalances={setBalances}/>
-            <br/>
+            <ClearingMemberBalance
+                balances={balances}
+                setBalances={setBalances}
+            />
+            <br />
 
             <h1 className="text-3xl">Display</h1>
-            <DisplayObjects trades={trades}/>
+            <DisplayObjects trades={trades} />
             <CorporateActionButton />
-
         </>
     );
 }
